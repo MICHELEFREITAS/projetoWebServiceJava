61ec9045054aadf8c08979df9c17566e9640c5bd
MICHELEFREITAS <michele-braga@hotmail.com>
2019-09-25 11:55:29 +0100

    Many-to-many association with JoinTable

--- src/main/java/com/michelefreitas/projetoWebService/entities/Product.java
+++ src/main/java/com/michelefreitas/projetoWebService/entities/Product.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -29,7 +31,11 @@ public class Product implements Serializable {
 	//o mesmo produto não pode ter uma mesma categoria mais de uma vez
 	//Dentro produto tenho conjunto de Categorias
 	//instancia para a coleção não comece valendo nula.Começa vazia porém estanciada. O set é uma interface não pode ser instanciado por isso usa o HashSet.
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	           joinColumns = @JoinColumn(name="product_id"),
+	           inverseJoinColumns = @JoinColumn(name="category_id"))//nome tabela no BD e chave estrangeira que vai associar produto com categoria e o inverse que define chave estrangeira da outra entidade
+	
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
